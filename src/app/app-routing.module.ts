5fdf4e02bd580dc117a44f7c856fa0315624840e
chandrika <udhayachandrikam31@gmail.com>
2021-10-07 16:35:41 +0530

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { RegistrationComponent } from './forms-module/registration/registration.
 import { WardenDetailsComponent } from './forms-module/registration/warden-details/warden-details.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CommodityMasterComponent } from './master/commodity-master/commodity-master.component';
 import { FoodmasterComponent } from './master/foodmaster/foodmaster.component';
 import { HostelmasterComponent } from './master/hostelmaster/hostelmaster.component';
+import { OpeningBalanceComponent } from './opening-balance/opening-balance.component';
 import { AuthGuard } from './services/auth.guard';
 
 
