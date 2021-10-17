1833396ab7b3e02b2a8a0696331f9879e53504f4
chandrika <udhayachandrikam31@gmail.com>
2021-10-17 23:56:26 +0530

    registration changes and consumption completed code

--- src/app/services/master-data.service.ts
+++ src/app/services/master-data.service.ts
@@ -122,7 +122,7 @@ export class MasterService {
             //role master
             case 'RM':
                 this.data.Table13.forEach(r => {
-                    this.masterData.push({ name: r.Name, code: r.Id });
+                    this.masterData.push({ name: r.Name, code: r.RoleId });
                 })
                 break;
             //commodity master
