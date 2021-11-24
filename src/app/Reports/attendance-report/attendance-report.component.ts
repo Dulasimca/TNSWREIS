68874fd95bbba16de6768044efcff687199e519e
Aditiya_T661 <Aditiya_T661@TANDVCL0041>
2021-11-24 19:28:19 +0530

    attendance report changes added

--- src/app/Reports/attendance-report/attendance-report.component.ts
+++ src/app/Reports/attendance-report/attendance-report.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
 import { MessageService, SelectItem } from 'primeng/api';
-import { HttpClient  } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { PathConstants } from 'src/app/Common-Modules/PathConstants';
 import { RestAPIService } from 'src/app/services/restAPI.service';
 import { MasterService } from 'src/app/services/master-data.service';
@@ -18,249 +18,165 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./attendance-report.component.css']
 })
 export class AttendanceReportComponent implements OnInit {
-
   fromdate: Date = new Date();
-  _districtname:string
-  _taluknname:string
-  _hostel:string
-  attdrepthostelname:any
-  districtOptions:SelectItem[];
-  talukOptions:SelectItem[];
-  Districtcode: number;
-  Districtcodes?: any;
-  TalukId: number;
-  TalukIds?: any;
-  disableTaluk: boolean = true;
-  data:any;
-  attendanceImgdata : any
-  todate:Date = new Date();
+  districtOptions: SelectItem[];
+  talukOptions: SelectItem[];
+  district: string;
+  districts?: any;
+  taluk: string;
+  taluks?: any;
+  data: any;
+  attendanceImgdata: any
+  todate: Date = new Date();
   cols: any;
   colsAttendance: any;
   hostelName: number;
   hostels?: any;
   hostelOptions: SelectItem[];
   role: number;
-  districts?: any;
-  taluks?: any;
-  
-  showDistrict: boolean;
-  showTaluk: boolean;
-  showHostelName: boolean;
   checkEmail: boolean;
-  isDistrict : boolean;
-  isTaluk:boolean;
-  isHostel:boolean;
   login_user: User;
-  showDialog : boolean;
-  showDialogLargeImg : boolean;
-  ImageUrl : string;
+  showDialog: boolean;
+  showDialogLargeImg: boolean;
+  ImageUrl: string;
 
   constructor(private http: HttpClient, private restApiService: RestAPIService,
-    private masterService: MasterService,private messageService: MessageService,private datepipe: DatePipe,private authService: AuthService) { }
+    private masterService: MasterService, private messageService: MessageService, private datepipe: DatePipe, private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.showDialogLargeImg=false;
+    this.showDialogLargeImg = false;
     this.showDialog = false;
     this.login_user = this.authService.UserInfo;
-    this.Districtcode=this.login_user.districtCode;
-    this.TalukId=this.login_user.talukId;
-    this.hostelName=this.login_user.hostelId;
-    this.role=this.login_user.roleId;
-    this.selectHostelInfo();
-    this.Districtcodes = this.masterService.getMaster('DT');
-    this.TalukIds = this.masterService.getMaster('TK');
+    this.hostelName = this.login_user.hostelId;
+    this.role = this.login_user.roleId;
+    this.districts = this.masterService.getMaster('DT');
+    this.taluks = this.masterService.getMaster('TK');
     this.cols = [
-      {field:'AttendanceDate',header: 'Date'},
-      {field:'DistrictName',header: 'District Name'},
-      {field:'TalukName',header: 'Taluk Name'},
-      {field:'HostelName',header: 'Hostel Name'},
-      {field:'NOOfStudent',header: 'Total Student'},
-      {field:'Remarks',header: 'Remarks'},
+      { field: 'AttendanceDate', header: 'Date' },
+      { field: 'DistrictName', header: 'District Name' },
+      { field: 'TalukName', header: 'Taluk Name' },
+      { field: 'HostelName', header: 'Hostel Name' },
+      { field: 'NOOfStudent', header: 'Total Student' },
+      { field: 'Remarks', header: 'Remarks' },
     ];
 
     this.colsAttendance = [
-      {field:'Uploaddate',header: 'Date'},
-      {field:'CreatedDate',header: 'System Date'},
+      { field: 'Uploaddate', header: 'Date' },
+      { field: 'CreatedDate', header: 'System Date' },
     ];
-
-    this.DataChangeBasedonRole();
   }
+
   onSelect(type) {
     let districtSelection = [];
     let talukSelection = [];
     let hostelSelection = [];
-    
-    switch (type) {   
+    switch (type) {
       case 'D':
-          this.Districtcodes.forEach(d => {
-            districtSelection.push({ label: d.name, value: d.code });
-          })
-          this.districtOptions = districtSelection;
+        this.districts.forEach(d => {
+          districtSelection.push({ label: d.name, value: d.code });
+        })
+        this.districtOptions = districtSelection;
+        if ((this.login_user.roleId * 1) !== 4) {
           this.districtOptions.unshift({ label: 'All', value: 0 });
-          if (this.Districtcode !== null && this.Districtcode !== undefined) {
-            this.disableTaluk = false;
-           } else {
-             this.disableTaluk = true; 
-           }
-          break;
-          case 'TK':
-            this.TalukIds.forEach(t => {
-              if (t.dcode === this.Districtcode){
-              talukSelection.push({ label: t.name, value: t.code });
-            }
-            });
-            this.talukOptions = talukSelection;
-            this.talukOptions.unshift({ label: 'All', value: 0 });
-            break;
-            case 'HN':
-              console.log(this.hostels);
-              this.hostels.forEach(h => {
-                hostelSelection.push({ label: h.HostelName, value: h.Slno });
-              })
-              this.hostelOptions = hostelSelection;
-              this.hostelOptions.unshift({ label: 'All', value: 0 });
-              break;
+        }
+        this.districtOptions.unshift({ label: '-select-', value: null });
+        break;
+      case 'TK':
+        this.taluks.forEach(t => {
+          if (t.dcode === this.district) {
+            talukSelection.push({ label: t.name, value: t.code });
+          }
+        });
+        this.talukOptions = talukSelection;
+        if ((this.login_user.roleId * 1) !== 4) {
+          this.talukOptions.unshift({ label: 'All', value: 0 });
+        }
+        this.talukOptions.unshift({ label: '-select-', value: null });
+        break;
     }
-    
   }
-  selectHostelInfo() {
-  let  hostelSelection = [];
-    const params = {
-      'Type': 1,
-      'Value': this.Districtcode
 
+  loadHostelList() {
+    let hostelSelection = [];
+    const params = {
+      'Type': 0,
+      'DCode': this.district,
+      'TCode': this.taluk,
+      'HostelId': ((this.login_user.roleId * 1) === 4) ? this.login_user.hostelId : 0
     }
-    if (this.Districtcode !== null && this.Districtcode !== undefined) {
+    if (this.district !== null && this.district !== undefined && this.district !== 'All' &&
+      this.taluk !== null && this.taluk !== undefined) {
       this.restApiService.getByParameters(PathConstants.Hostel_Get, params).subscribe(res => {
         if (res !== null && res !== undefined && res.length !== 0) {
-          if(this.role==4)
-          {
-           res.Table.forEach(d => {
-            if (d.Slno === this.hostelName){
-              hostelSelection.push({ label: d.HostelName, value: d.Slno });
-            }
-          });
-  
-            this.hostelOptions = hostelSelection;
-        }
-        else
-        {
           this.hostels = res.Table;
+          this.hostels.forEach(h => {
+            hostelSelection.push({ label: h.HostelName, value: h.Slno });
+          })
         }
-    
-        };
-
-      });
+      })
     }
+    this.hostelOptions = hostelSelection;
+    if ((this.login_user.roleId * 1) !== 4) {
+      this.hostelOptions.unshift({ label: 'All', value: 0 });
+    }
+    this.hostelOptions.unshift({ label: '-select-', value: null });
   }
 
-  DataChangeBasedonRole() {
-    console.log("Roleid",this.role,this.Districtcode )
-    console.log("District",this.Districtcodes )
-  
-    let districtSelection = [];
-    let talukSelection = [];
-    let hostelSelection = [];
-    if (this.role != undefined && this.role !== null) {
-      if (this.role === 4) {
-        this.Districtcodes.forEach(d => {
-          if (d.code === this.Districtcode){
-          districtSelection.push({ label: d.name, value: d.code });
-          }
-        });
-        this.districtOptions = districtSelection;
+  refreshFields(value) {
+    if (value === 'D') {
+      this.taluk = null;
+      this.talukOptions = [];
+    }
+    this.loadHostelList();
+  }
 
-        this.TalukIds.forEach(d => {
-          if (d.code === this.TalukId){
-            talukSelection.push({ label: d.name, value: d.code });
-          }
-        });
-        this.talukOptions = talukSelection;
-        this.isDistrict = true;
-        this.isTaluk = true;
-        this.isHostel = true;
-
-      } else if (this.role === 3) {
-        this.Districtcodes.forEach(d => {
-          if (d.code === this.Districtcode){
-          districtSelection.push({ label: d.name, value: d.code });
-          }
-        });
-        this.districtOptions = districtSelection;
-        this.isDistrict = true;
-        this.isTaluk = false;
-        this.isHostel = false;
-
-      } else if (this.role === 2) {
-        
-        this.Districtcodes.forEach(d => {
-          if (d.code === this.Districtcode){
-          districtSelection.push({ label: d.name, value: d.code });
-          }
-        });
-        this.districtOptions = districtSelection;
-
-
-        this.TalukIds.forEach(d => {
-          if (d.code === this.TalukId){
-            talukSelection.push({ label: d.name, value: d.code });
-          }
-        });
-        this.talukOptions = talukSelection;
-        this.isDistrict = false;
-        this.isTaluk = false;
-        this.isHostel = true;
-
-      } else if (this.role === 1) {
-        this.isDistrict = false;
-        this.isTaluk = false;
-        this.isHostel = false;
+  loadAttendance() {
+    this.data = [];
+    const params = {
+      'HostelID': this.hostelName != undefined && this.hostelName != null ? this.hostelName : 0,
+      'Districtcode': this.district != undefined && this.district != null ? this.district : 0,
+      'Talukid': this.taluk != undefined && this.taluk != null ? this.taluk : 0,
+      'FromDate': this.datepipe.transform(this.fromdate, 'MM/dd/yyyy'),
+      'Todate': this.datepipe.transform(this.todate, 'MM/dd/yyyy')
+    }
+    this.restApiService.getByParameters(PathConstants.Attendance_Get, params).subscribe(res => {
+      if (res !== null && res !== undefined) {
+        if (res.length !== 0) {
+          this.data = res.Table;
+        } else {
+          this.messageService.clear();
+          this.messageService.add({
+            key: 't-msg', severity: ResponseMessage.SEVERITY_WARNING,
+            summary: ResponseMessage.SUMMARY_WARNING, detail: ResponseMessage.NoRecForCombination
+          })
+        }
       } else {
-        this.isDistrict = false;
-        this.isTaluk = false;
-        this.isHostel = false;
+        this.messageService.clear();
+        this.messageService.add({
+          key: 't-msg', severity: ResponseMessage.SEVERITY_WARNING,
+          summary: ResponseMessage.SUMMARY_WARNING, detail: ResponseMessage.NoRecForCombination
+        })
       }
-    }
+    });
   }
 
- 
-
-  onview() {
-    const params={
-      'HostelID' : this.hostelName != undefined && this.hostelName != null ? this.hostelName : 0,	
-     'Districtcode'	: this.Districtcode != undefined && this.Districtcode != null ? this.Districtcode : 0 ,
-     'Talukid'		: this.TalukId != undefined && this.TalukId != null ? this.TalukId : 0 ,
-    'FromDate'		:this.datepipe.transform(this.fromdate,'MM/dd/yyyy'), 
-     'Todate'		:this.datepipe.transform(this.todate,'MM/dd/yyyy')
+  showImage(Id) {
+    this.showDialog = true;
+    const params = {
+      'AttendanceId': Id != undefined && Id != null ? Id : 0
     }
-    this.restApiService.getByParameters(PathConstants.Attendance_Get,params).subscribe(res => {
-     if(res !== null && res !== undefined && res.length !==0) {
-       this.data = res.Table;
-     }
-
-    
-   });
-
-}
-showImage(Id)
-{
-  this.showDialog = true;
-  const params={
-    'AttendanceId' : Id != undefined &&Id != null ? Id : 0
+    this.restApiService.getByParameters(PathConstants.AttendanceImageDetails_Get, params).subscribe(res => {
+      if (res !== null && res !== undefined && res.length !== 0) {
+        res.Table.forEach(i => {
+          i.url = 'assets/layout/' + i.HostelID + '/' + i.ImageName;
+        });
+        this.attendanceImgdata = res.Table;
+      }
+    });
   }
-  this.restApiService.getByParameters(PathConstants.AttendanceImageDetails_Get,params).subscribe(res => {
-   if(res !== null && res !== undefined && res.length !==0) {
-     res.Table.forEach(i => {
-      i.url = 'assets/layout/'+i.HostelID +'/' + i.ImageName;
-     });
-     this.attendanceImgdata = res.Table;
-   }
- });
-}
-LargeImage(url)
-{
-  this.showDialogLargeImg = true;
-  this. ImageUrl = url
 
-}
+  LargeImage(url) {
+    this.showDialogLargeImg = true;
+    this.ImageUrl = url
+  }
 }
