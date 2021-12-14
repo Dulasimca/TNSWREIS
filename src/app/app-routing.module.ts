8f0591890c64b320edc729b7cbe2af0ced2ba518
chandrika31 <udhayachandrikamurugan94@gmail.com>
2021-12-14 16:34:48 +0530

    student transfer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -53,7 +53,7 @@ const routes: Routes = [
   { path: 'hostelmaster',component:HostelmasterComponent, canActivate: [AuthGuard]},
   { path: 'registration', component: RegistrationComponent, canActivate: [AuthGuard] },
   { path: 'warden-details', component: WardenDetailsComponent, canActivate: [AuthGuard]}, 
-  { path: 'opening-balance', component: OpeningBalanceComponent, canActivate: [AuthGuard]},
+  { path: 'opening-balance', component: StudentTransferFormComponent, canActivate: [AuthGuard]},
   { path: 'student-transfer', component: StudentTransferFormComponent, canActivate: [AuthGuard]},
   { path: 'commodity-master', component: CommodityMasterComponent, canActivate: [AuthGuard]},
   { path: 'user-master', component:UsermasterComponent, canActivate: [AuthGuard]},
