8c1ced84adc6ae113268eb3d651258f39d884763
sivamani mohan <sivamanimohan@outlook.com>
2022-02-15 07:31:46 -0800

    Attendance checking

--- src/app/forms-module/attendance-image/attendance-image.component.ts
+++ src/app/forms-module/attendance-image/attendance-image.component.ts
@@ -196,6 +196,8 @@ export class AttendanceImageComponent implements OnInit {
           this.attendanceId = 0;
         }
       });
+      //new line added
+      this.blockUI.stop();
     }
 
   }
