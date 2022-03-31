ddf6e37147003fdcd48994665d7fe142a83c079d
Aditiya_T661 <Aditiya_T661@TANDVCL0041>
2022-03-31 12:52:58 +0530

    idle timeout added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { AuthService } from './services/auth.service';
+import { IdleService } from './services/idle.service';
 import { MasterService } from './services/master-data.service';
 
 @Component({
@@ -14,11 +16,21 @@ export class AppComponent {
   hideHeader: boolean = false;
   isLoggedIn: boolean = false;
 
-  constructor(private _authService: AuthService) {
+  constructor(private _authService: AuthService, private router: Router, private idleService: IdleService) {
     this._authService.isLoggedIn.subscribe(value => {
       this.isLoggedIn = value;
     });
   }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.initialIdleSettings();
+  }
+  private initialIdleSettings() {
+    const idleTimeoutInSeconds: number = environment.idleTimeInMinutes * 60;
+    this.idleService.startWatching(idleTimeoutInSeconds).subscribe((isTimeOut: boolean) => {
+      if (isTimeOut) {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
