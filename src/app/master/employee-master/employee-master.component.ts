0d8a8489ee791547fa62a00c78c6619d64533dbc
Adithya_10010 <Adithya_10010@TANDVBL0037>
2022-06-28 11:29:19 +0530

    app status online reg emp master changes added

--- src/app/master/employee-master/employee-master.component.ts
+++ src/app/master/employee-master/employee-master.component.ts
@@ -80,7 +80,7 @@ export class EmployeeMasterComponent implements OnInit {
   ngOnInit(): void {
 
     const current_year = new Date().getFullYear();
-    const start_year_range = current_year - 50;
+    const start_year_range = current_year - 72;
     this.yearRange = start_year_range + ':' + current_year;
 
     this.cols = [
