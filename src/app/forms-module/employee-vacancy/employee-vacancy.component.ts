8b447045af05959ec509fff32161979e31cf8d63
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-07-15 11:08:10 +0530

    new reports & form added

---
+++ src/app/forms-module/employee-vacancy/employee-vacancy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-vacancy',
+  templateUrl: './employee-vacancy.component.html',
+  styleUrls: ['./employee-vacancy.component.css']
+})
+export class EmployeeVacancyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
